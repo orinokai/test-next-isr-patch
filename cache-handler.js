19ee63cf092f3b8be2ec07a65f2779180dc33aa4
Rob Stanford <me@robstanford.com>
2023-06-09 17:23:52 +0100

    add cache handler

---
+++ cache-handler.js
@@ -0,0 +1,17 @@
+module.exports = class IncrementalCache {
+  async revalidateTag() {
+    return
+  }
+
+  async fetchCacheKey() {
+    return ''
+  }
+
+  async get() {
+    return null
+  }
+
+  async set() {
+    return
+  }
+}

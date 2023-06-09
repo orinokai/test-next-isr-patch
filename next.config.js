bb87fc00c6825b17d10a91cf5b79cf407c97e708
Rob Stanford <me@robstanford.com>
2023-06-09 17:24:04 +0100

    config cache handler

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    incrementalCacheHandlerPath: '../cache-handler.js'
+  }
 }
 
 module.exports = nextConfig
